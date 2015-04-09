f3563f37dcbaa2d5084eb1ba9d1bb8035b8ab51f
Marc Jones <marc.jones@jic.ac.uk>
2015-04-09 14:22:11 +0100

    Changed the form submission website
    forms.braces.io was no longer online, so I found an equivalent
    alternative. I also changed the Google Doc URL to a spreadsheet
    of mine.

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    var formUrl = 'https://docs.google.com/forms/d/1HVcQflbgw1bexE3BxLMJwwqwR8TlVyLRVxfOb-SMoEA/formResponse';
+    var formUrl = 'https://docs.google.com/forms/d/1CmXZERcW5xw_BvVLGupVl2JpqdRej0Wrs3jmSrOUuec/formResponse';
 
     // Handle form submission
     $('form').submit(function(e) {
